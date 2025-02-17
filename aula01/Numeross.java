f705bf17e9d7cee3c96c44dac4f098e37e4e8fec
Rafael Serafim <rafaelserafimcalzzetta@gmail.com>
2025-02-17 09:19:00 -0300

    Aula sobre estrutura e códigos básicos em Java

---
+++ aula01/Numeross.java
@@ -0,0 +1,9 @@
+public class Numeross{
+    public static void main(String[] args){
+        System.out.println(3+6);
+        System.out.println(5*2);
+        System.out.println(5-4);
+        System.out.println(6/2);
+    }
+    
+    }
