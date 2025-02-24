91f6823eebc7b366cbf2cb79468623c71f69f8dd
Rafael Serafim <rafaelserafimcalzzetta@gmail.com>
2025-02-24 09:30:59 -0300

    Aula de java

--- aula01/Main.java
+++ aula01/Main.java
@@ -1,3 +1,4 @@
+
 public class Main {
     public static void main(String[] arg) {
         System.out.println("Hello World");
---
+++ aula02/Variaveis.java
@@ -0,0 +1,10 @@
+ public class Variaveis {
+    public static void main(String[] args){
+        String msg = "John";
+        System.out.println(msg);
+        int n1= 3;
+        int n2 = 4;
+        
+        System.out.print(n2 + n1);
+    }
+}
