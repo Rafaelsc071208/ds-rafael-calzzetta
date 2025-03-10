39333c4957e197d1e937fee8dfea05f7d5434308
Rafael Serafim <rafaelserafimcalzzetta@gmail.com>
2025-03-10 09:10:26 -0300

    Testes com variaveis

---
+++ aula03/Testeconversao.java
@@ -0,0 +1,9 @@
+public class Testeconversao {
+    public static void main(String[] args) {
+int max = 2000;
+int pla = 1450;
+float porcentagem = (float)pla/max*100;
+System.out.println((porcentagem)+("%"));
+        
+    }
+}
---
+++ aula03/Tiposvariaveis.java
@@ -0,0 +1,22 @@
+public class Tiposvariaveis {
+    public static void main(String[] args) {
+        int myNum = 5;
+        String name = "Rafael";
+        boolean myBool=true;
+        float myflo = 3.5f;
+        char mycha = 'R';
+        double mydou = 8.75;
+        int myint = (int) mydou;
+       
+        
+        
+        System.out.println(myNum);
+        System.out.println(name);
+        System.out.println(myBool);
+        System.out.println(myflo);
+        System.out.println(mycha);
+        System.out.println((int)mydou);
+        System.out.println(mydou);
+        
+    }
+}
