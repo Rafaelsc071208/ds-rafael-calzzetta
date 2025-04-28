0638b86eb699db9808e501c3df629f0cc89ddb34
Rafael Serafim <rafaelserafimcalzzetta@gmail.com>
2025-04-28 08:46:59 -0300

    Aula sobre o uso do netbeans e exercicios

---
+++ aula05/src/aula05/Exercicio01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula05;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Exercicio01 {
+        public static void main(String[] args) {
+
+    int jan = 15000;
+    int fev = 23000;
+    int mar = 17000;
+    
+    int total = jan+fev+mar;
+    
+            System.out.println("O balanço trimestral é "+total+"");
+        }
+}
